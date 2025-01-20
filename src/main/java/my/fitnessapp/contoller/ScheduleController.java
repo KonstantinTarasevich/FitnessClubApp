feb0d8a55c083344acedb09c171c8dedf3bd81b1
KonstantinTarasevich <134845899+KonstantinTarasevich@users.noreply.github.com>
2025-01-20 19:18:54 +0200

    Added count of users functionality

--- src/main/java/my/fitnessapp/contoller/ScheduleController.java
+++ src/main/java/my/fitnessapp/contoller/ScheduleController.java
@@ -1,13 +1,10 @@
 package my.fitnessapp.contoller;
 
 
-import my.fitnessapp.model.dto.ScheduleDTO;
 import my.fitnessapp.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("schedules")
@@ -21,27 +18,9 @@ public class ScheduleController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ScheduleDTO>> getAllSchedules() {
-        return ResponseEntity.ok(scheduleService.getAllSchedulesOrderedByDateTime());
+    public String getAllSchedules(Model model) {
+
+        model.addAttribute("allSchedules", scheduleService.getAllSchedulesOrderedByDateTime());
+        return "schedule";
     }
 }
-
-
-//    @GetMapping("/create")
-//    public String showCreateForm(Model model) {
-//        model.addAttribute("schedule", new ScheduleDTO());
-//        return "schedule";
-//    }
-//
-//    @PostMapping("/create")
-//    public String createSchedule(@ModelAttribute ScheduleDTO scheduleDTO) {
-//        scheduleService.createSchedule(scheduleDTO);
-//        return "redirect:/schedules";
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public String deleteSchedule(@PathVariable Long id) {
-//        scheduleService.deleteSchedule(id);
-//        return "redirect:/schedules";
-//    }
-//}
